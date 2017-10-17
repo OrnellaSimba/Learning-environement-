4204a09b31051f3504dc666e3890b98d8ae4253b
OrnellaSimba <ornellasimba@gmail.com>
2017-10-17 09:36:41 +0200

    Learning js

---
+++ JavaScript/Exo1-variable/exo1.js
@@ -0,0 +1,15 @@
+var fr='français';
+var en='anglais';
+var es='espagnole';
+var langue= es;
+
+
+if (langue==fr) {
+  console.log("hello tout le monde");
+}
+else if (langue==en) {
+  console.log("hello world");
+}
+else if (langue==es) {
+  console.log("hola mundo");
+}
